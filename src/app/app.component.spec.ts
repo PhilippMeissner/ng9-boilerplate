432197c10b1ee63300e8cb726289a08cc20957d5
philippmeissner <pm@zeit.io>
2020-04-13 14:51:55 +0200

    used headless chrome for tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ng9-boilerplate');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng9-boilerplate app is running!');
-  });
 });
