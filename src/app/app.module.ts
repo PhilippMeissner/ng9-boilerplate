3450cb16d3c4dc436f341069709264cd6fabbcd5
philippmeissner <pm@zeit.io>
2020-04-13 15:17:32 +0200

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {LoadingBarModule} from '@ngx-loading-bar/core';
 import {LoadingBarHttpClientModule} from '@ngx-loading-bar/http-client';
 import {LoadingBarRouterModule} from '@ngx-loading-bar/router';
@@ -17,6 +19,9 @@ import {AppComponent} from './app.component';
     LoadingBarHttpClientModule,
     LoadingBarRouterModule,
     LoadingBarModule,
+    BrowserAnimationsModule,
+
+    MatButtonModule,
   ],
   bootstrap: [AppComponent],
 })
